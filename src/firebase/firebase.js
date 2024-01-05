cab8ab8b525d4dadf500dafae182841ac9328fa4
hnjog <43630972+hnjog@users.noreply.github.com>
2024-01-05 17:58:20 +0900

    test

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCyBN1cZne8YcStBqy5BM_q2Xvaz8QtyWM",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
